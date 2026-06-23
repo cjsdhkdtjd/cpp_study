9913b7cbde88eb944db59e644892145105817915
cjsdhkdtjd <155054423+cjsdhkdtjd@users.noreply.github.com>
2026-06-23 14:39:05 +0900

    .

--- Project1/practice1.cpp
+++ Project1/practice1.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 
 int main() {
-	float x, y;
-	cout << "키를 입력하세요: ";
-	cin >> x;
-	cout << "몸무게를 입력하세요: ";
-	cin >> y;
-	cout << "키(cm): " << x << endl;
-	cout << "몸무게(kg): " << y << endl;
-	x = x / 100;
-	cout << "BMI: " << y / (x*x) << endl;
+	int num;
+	cout << "정수를 입력하시오: ";
+	cin >> num;
+
+	for (int i = 1; i <= 9; i++) {
+		cout << num << " x " << i << " = " << num * i << endl;
+	}
+	return 0;
 }
