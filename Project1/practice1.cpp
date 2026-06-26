12dbecc6a073cc40c115222d59e38e60b08fbdf3
cjsdhkdtjd <155054423+cjsdhkdtjd@users.noreply.github.com>
2026-06-26 13:05:28 +0900

    .

--- Project1/practice1.cpp
+++ Project1/practice1.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main() {
-	int arr[3] = { 10, 20, 30 };
+	int a = 42;
+	int* p = &a;
 
-	for (int i = 0; i < 3; i++) {
-		cout << "arr[" << i << "] = " << arr[i] << endl;
-	}
+	cout << "a의 값: " << a << endl;
+	cout << "a의 주소: " << &a << endl;
+	cout << "포인터 p의 값(=a의 주소): " << p << endl;
+	cout << "포인터가 가리키는 값: " << *p << endl;
+
 	return 0;
 }
