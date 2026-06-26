19329a24c1e98a54418ac0d7a39a91348971db11
cjsdhkdtjd <155054423+cjsdhkdtjd@users.noreply.github.com>
2026-06-26 13:10:48 +0900

    .

--- Project1/practice1.cpp
+++ Project1/practice1.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main() {
-	int a = 42;
-	int* p = &a;
+	int a = 100;
+	int& ref = a;
 
-	cout << "a의 값: " << a << endl;
-	cout << "a의 주소: " << &a << endl;
-	cout << "포인터 p의 값(=a의 주소): " << p << endl;
-	cout << "포인터가 가리키는 값: " << *p << endl;
+	cout << "a = " << a << endl;
+	cout << "ref = " << ref << endl;
+
+	ref = 200;
+	cout << "a = " << a << endl;
 
 	return 0;
 }
